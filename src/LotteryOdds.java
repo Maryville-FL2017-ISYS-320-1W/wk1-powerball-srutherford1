f2863d14b8a7b55a159d99bdac3d8b11777e3350
Samantha Rutherford <33076449+srutherford1@users.noreply.github.com>
2017-11-05 22:08:18 -0500

    Update LotteryOdds.java

--- src/LotteryOdds.java
+++ src/LotteryOdds.java
@@ -1,29 +1,33 @@
+/**
+ * ISYS-320-w1
+ * author: Samantha Rutherford
+ * date: November 5th, 2017
+ **/
+
 import java.math.BigInteger;
+import java.util.Scanner;
 
-public class LotteryOdds {
+public class Lottery {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		int r;
-		int w = 10; // 69
-		int s = 2; // 5
-		int pb = 26;
-	    r = factorial(w) / (factorial(s) * (factorial(w-s)));  // Superduper magic equation!!
-	    r = r * pb;
-	    
-	    System.out.println("The odds of winning the PowerBall are 1:" + r);
-	    System.out.println("The PowerBall website says that it's 1:292201338");
-	    
-    }
+      Scanner scan = new Scanner(System.in);
+      int poolSize = 30;
 
-	private static Integer factorial(int n) {
-	    int i = 1;
-	    int f = 1;
-	 
-	    for (int c = 1; c <= n; c++) {
-	      f = f * i++;
-	    }
+      // Prompt user to input the numbers to draw for the lottery.
+      System.out.print("Choose the numbers to draw, bro!\n Nothing greater than 5:\t");
+      int n = scan.nextInt(); //to input integer between 1-5.
+      if (n <= 5)
+      {
+          BigInteger Lottery = BigInteger.valueOf(1);
+          for (int i=1; i<=n; i++)
+            Lottery = Lottery.multiply(BigInteger.valueOf(poolSize - i + 1)).divide(BigInteger.valueOf(i));
+          System.out.println("The odds of winning the lottery are 1 in " + Lottery + ". Good luck! You'll lose. Its rigged... xD")
+      }
+      else
+        System.out.println("Sorry. I said nothing greater than 5! You lose.")
 
-		return f;
-	}
+
+  }
+
 }
